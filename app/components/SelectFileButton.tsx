f7ae2fa502540e297119efe39a7a98311632f058
its-helloant <itshelloant@gmail.com>
2025-11-14 19:38:13 -0600

    image preview done, styling needed

--- app/components/SelectFileButton.tsx
+++ app/components/SelectFileButton.tsx
@@ -1,30 +1,19 @@
 'use client';
 
-import React, { useState, useRef } from "react";
+import React, { useRef } from "react";
 import { Input } from "@/components/ui/input"
 
-export function SelectFileButton() {
+export function SelectFileButton({ onFileChange }: { onFileChange: (event: React.ChangeEvent<HTMLInputElement>) => void }) {
   const fileInputRef = useRef<HTMLInputElement>(null)
 
-  const handleClick = () => {
-    fileInputRef.current?.click();
-  }
-
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    // if (file) {
-    //   setButtonText(`File Selected: ${file.name}`);
-    // }
-  }
-
   return (
-    <div className="flex flex-wrap items-center gap-2 md:flex-row">
+    <div className="place-content-center">
       <Input 
       ref={fileInputRef} 
       id="picture" 
       type="file" 
       accept="image/*" 
-      onChange={handleFileChange} 
+      onChange={onFileChange} 
       className='text-muted-foreground file:border-input file:text-foreground p-0 pr-3 italic file:mr-3 file:h-full file:border-0 file:border-r file:border-solid file:bg-transparent file:px-3 file:text-sm file:font-medium file:not-italic'
       />
     </div>  
